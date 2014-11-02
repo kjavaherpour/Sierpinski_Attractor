93177ced03376da8bfe57a639cfd76750893a2c5
Ricky Sidhu <ricky@dearsaturn.com>
2014-11-02 15:29:46 -0800

    Shapes Placed, Classes Setup
    - Shapes take color from the ComboBox.
    - SAShape class created
    - Listeners Setup
    - This project can be opened in Blend (to add more listeners) as well as
    VS (to do all the coding), at the same time.

--- Sierpinski_Triangles/MainWindow.xaml.cs
+++ Sierpinski_Triangles/MainWindow.xaml.cs
@@ -20,9 +20,89 @@ namespace Sierpinski_Triangles
     /// </summary>
     public partial class MainWindow : Window
     {
+        SAShape[] shape = new SAShape[2000];
+        int controlPoints = -1;
         public MainWindow()
+        {            
+            InitializeComponent();            
+        }
+
+        /// <summary>
+        /// Places control points
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShapeCanvas_MouseRightButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            InitializeComponent();
+            controlPoints++;
+            //No more than 6 control points are allowed.
+            if (controlPoints > 6) controlPoints--;
+            else
+            {
+                Rectangle rect = new Rectangle();
+                rect.Name = "CP_" + controlPoints.ToString(); //Control Point # assignment, starts at 0
+                rect.Height = 10;
+                rect.Width = 10;
+                rect.Stroke = Brushes.Black;
+
+                //Get the selected ComboBox item for shape color
+                if (ComboBox.GetIsSelected(RedShapeItem))
+                {
+                    rect.Fill = Brushes.Red;
+                }
+                if (ComboBox.GetIsSelected(GreenShapeItem))
+                {
+                    rect.Fill = Brushes.Green;
+                }
+                if (ComboBox.GetIsSelected(BlueShapeItem))
+                {
+                    rect.Fill = Brushes.Blue;
+                }
+                if (ComboBox.GetIsSelected(YellowShapeItem))
+                {
+                    rect.Fill = Brushes.Yellow;
+                }
+
+                //Get the position from the event firing it, relative to (on) the canvas
+                Canvas.SetLeft(rect, e.GetPosition(ShapeCanvas).X);
+                Canvas.SetTop(rect, e.GetPosition(ShapeCanvas).Y);
+                ShapeCanvas.Children.Add(rect);
+                MessageBox.Show("Added Shape " + rect.Name, "Shape Added");
+            }
+
+        }
+
+        /// <summary>
+        /// Drag the control points around
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShapeCanvas_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+        }
+
+        /// <summary>
+        /// Clear all the shapes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ShapeCanvas.Children.Clear();
+            controlPoints = -1;
+        	// TODO: Add event handler implementation here.
+        }
+
+        /// <summary>
+        /// Create the attractor
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RunButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+            // TODO: Add event handler implementation here.
         }
     }
 }
---
+++ Sierpinski_Triangles/SAShape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace Sierpinski_Triangles
+{
+    /// <summary>
+    /// An extension of the Rectangle class purposed for the sake of the algorithm.
+    /// </summary>
+    class SAShape : Rectangle
+    {
+        public SAShape()
+        {
+
+        }
+        public void setPosition(double left, double top){
+
+        }
+    }
+}
